ae12809c584326c1d753fe1e7354c2353ee1edad
Park JiWeon <kor98won@gmail.com>
2022-06-20 18:33:39 +0900

    관심사의 분리_10:05

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,12 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     // 이게 null이면 다른 메소드 사용 시 널포인터 예외 뜰 수 있으므로 구현체를 선택해줘야 한다.
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // 생성자 주입 방식으로 변경
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
