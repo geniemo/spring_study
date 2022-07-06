75fec01c80b73d6c71b4934ff36b4f5d6299e97c
geniemo <kor98won@gmail.com>
2022-07-06 11:24:04 +0900

    상품 리포지토리 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { // 아직 DB에 등록되지 않으면 id가 null
+            em.persist(item);
+        } else { // DB에 등록되어 있는 걸 업데이트
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
