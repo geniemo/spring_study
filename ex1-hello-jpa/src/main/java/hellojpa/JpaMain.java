229eb3549d5e28a6ec4e338c00a172a098670a8f
geniemo <kor98won@gmail.com>
2022-07-10 20:10:21 +0900

    기본 키 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,102 +20,17 @@ public class JpaMain {
 
         // 엔티티 매니저를 꺼낸 후 여기서 실제 코드를 작성
         try {
-            /*
             // 삽입
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
+            Member memberA = new Member();
+            memberA.setUsername("HelloA");
 
-            em.persist(member);
+            Member memberB = new Member();
+            memberB.setUsername("HelloB");
 
-            // 조회
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getName() = " + findMember.getName());
+            em.persist(memberA);
+            em.persist(memberB);
 
-            // 삭제
-            Member findMember = em.find(Member.class, 1L);
-            em.remove(findMember);
-
-            // 수정
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
-            // persist로 저장하지 않아도 더티 체킹을 통해 변화를 감지, 반영
-             */
-
-            /*
-            // DB에 데이터가 있을 때 아래 두 줄 빼고 주석처리하고 실행해보자.
-            // 아래 두 줄 실행 결과를 보면 select문은 한 번만 나갔다.
-            // 첫 조회 시에는 select 문을 날려서 받아온거고 두 번째 조회 시에는 1차 캐시에서 가져왔기 때문이다.
-            Member findMember1 = em.find(Member.class, 100L);
-            Member findMember2 = em.find(Member.class, 100L);
-            System.out.println("(findMember1 == findMember2) = " + (findMember1 == findMember2));
-             */
-
-            /*
-            // 비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
-
-            // 영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member); // BEFORE와 AFTER 사이 출력에는 쿼리문이 없다. 여기서 쿼리문이 날라가지 않았다는 것
-            System.out.println("=== AFTER ===");
-            
-            // 출력을 확인해보면 조회를 했는데 select 쿼리가 안나갔다.
-            // 1차 캐시에 저장되어 있던 엔티티를 가져온 것
-            Member findMember = em.find(Member.class, 100L);
-
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getName() = " + findMember.getName());
-             */
-
-            /*
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
-
-            em.persist(member1);
-            em.persist(member2);
-            System.out.println("=================="); // 이 이전에는 쿼리가 나가지 않는다.
-             */
-
-            /*
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
-
-            // 엔티티 수정 시 persist를 써서 얻을 수 있는 이점이 없다.
-            // 찾아 온 다음 데이터를 변경한 후 커밋해서 변경 감지를 해서 변경사항을 반영해준다.
-//            em.persist(member);
-            System.out.println("==================");
-             */
-
-            /*
-            Member member = new Member(200L, "member200");
-            em.persist(member);
-            
-            // 원래는 커밋 시에 날라가야 하지만 미리 확인을 해보고 싶다 -> 강제로 호출
-            em.flush();
-            System.out.println("==================");
-             */
-
-            /*
-            // 영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
-            
-            // 준영속
-//            em.detach(member);
-
-//            em.clear(); // 초기화 시에는 다시 조회 시 똑같은 애를 다시 select 문을 날려 조회하게 된다.
-//            Member member2 = em.find(Member.class, 150L);
-
-//            em.close(); // 이 경우에는 update가 된다. https://www.inflearn.com/questions/53733 참고하자.
-
-            System.out.println("=================="); // 이후 업데이트 쿼리가 나가지 않는다.
-             */
-
-            tx.commit(); // 쿼리는 트랜잭션을 커밋하는 시점에 영속성 컨텍스트에 있는 애가 DB에 날라가게 된다.
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,34 +4,33 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     // 객체는 username이라 쓰고싶은데 DB 컬럼 명은 name이라고 쓰고 싶은 경우
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String username;
 
-    private Integer age;
+    public Long getId() {
+        return id;
+    }
 
-    // 객체에서 enum type을 쓰고 싶은데 DB에는 enum 타입이 없다.
-    // 이 경우에 @Enumerated 어노테이션을 사용
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    // 날짜 타입을 쓰고싶은 경우 @Temporal을 쓰고
-    // DATE, TIME, TIMESTAMP 중 사용
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
+    public String getUsername() {
+        return username;
+    }
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    // VARCHAR를 넘어서는 큰 컨텐츠를 넣고 싶다면 @Lob 어노테이션 사용
-    // Lob이라 하고 문자 타입이면 DB 타입은 clob으로 된다.
-    @Lob
-    private String description;
-
-
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
