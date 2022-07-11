64d821186055e073d1894bf6dc3ff19ddfad6101
geniemo <kor98won@gmail.com>
2022-07-11 15:29:07 +0900

    양방향 연관관계와 연관관계의 주인 2 - 주의점, 정리

--- java_ORM_JPA-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ java_ORM_JPA-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -28,15 +28,20 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team);
+            member.changeTeam(team);
             em.persist(member);
 
+//            team.getMembers().add(member); // changeTeam을 만듦으로써 더이상 하지 않아도 된다.
+
+            // team.getMembers().add(member); 를 해주지 않았을 때
+            // em.flush, em.clear를 해주면 db에 들어가면서 id 같은 것들이 다 생성되기 때문에
+            // 아래에서 find 했을 때 정상적으로 팀과 멤버스 모두 잘 조회된다.
+            // 하지만 em.flush, em.clear를 해주지 않았다면 id들이 생성되지 않았기 때문에
+            // find를 했을 때 Team이 조회가 되긴 되지만 getMembers()의 결과로 나온 리스트에 아무것도 담겨 있지 않게 된다.
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam.getName() = " + findTeam.getName());
+            Team findTeam = em.find(Team.class, team.getId());
             List<Member> members = findTeam.getMembers();
             for (Member m : members) {
                 System.out.println("m.getUsername() = " + m.getUsername());
