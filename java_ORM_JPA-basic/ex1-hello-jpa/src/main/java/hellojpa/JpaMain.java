dbb3963ea5594506b831de861206e2e5ed0cbe6d
geniemo <kor98won@gmail.com>
2022-07-11 17:01:46 +0900

    다대일 [N:1]
    다대일 버전 보존

--- java_ORM_JPA-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ java_ORM_JPA-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -22,31 +22,6 @@ public class JpaMain {
         // 엔티티 매니저를 꺼낸 후 여기서 실제 코드를 작성
         try {
 
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team); // 영속 상태로
-
-            Member member = new Member();
-            member.setUsername("member1");
-            member.changeTeam(team);
-            em.persist(member);
-
-//            team.getMembers().add(member); // changeTeam을 만듦으로써 더이상 하지 않아도 된다.
-
-            // team.getMembers().add(member); 를 해주지 않았을 때
-            // em.flush, em.clear를 해주면 db에 들어가면서 id 같은 것들이 다 생성되기 때문에
-            // 아래에서 find 했을 때 정상적으로 팀과 멤버스 모두 잘 조회된다.
-            // 하지만 em.flush, em.clear를 해주지 않았다면 id들이 생성되지 않았기 때문에
-            // find를 했을 때 Team이 조회가 되긴 되지만 getMembers()의 결과로 나온 리스트에 아무것도 담겨 있지 않게 된다.
-            em.flush();
-            em.clear();
-
-            Team findTeam = em.find(Team.class, team.getId());
-            List<Member> members = findTeam.getMembers();
-            for (Member m : members) {
-                System.out.println("m.getUsername() = " + m.getUsername());
-            }
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
