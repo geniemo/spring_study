3b0705844a3aaa8efa0b5eae726958dd5fbe89e1
geniemo <kor98won@gmail.com>
2022-08-01 23:20:57 +0900

    간단한 주문 조회 V1: 엔티티를 직접 노출

--- jpashop2/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop2/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,23 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		//강제 지연 로딩 설정, 아래 옵션을 끄면 /api/v1/orders 에 있는 것처럼 for문으로 LAZY 강제 초기화를 해줘야 한다.
+		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
 	}
 
 }
---
+++ jpashop2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * ManyToOne, OneToOne 에서의 성능 최적화 살펴보기
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        // LAZY 강제 초기화
+        // order -> member 와 order -> address 는 지연 로딩이라서 실제 엔티티 대신 프록시 존재
+        // jackson 라이브러리는 기본적으로 이 프록시 객체를 json으로 어떻게 생성해야 하는지 모름 -> 예외 발생
+        // Hibernate5Module 을 스프링 빈으로 등록하면 해결 (스프링 부트 사용 중)
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
