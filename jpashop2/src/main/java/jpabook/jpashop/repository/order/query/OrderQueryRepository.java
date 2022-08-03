c66168f3e71c0c13a89c364221b3d48161ea50c5
geniemo <kor98won@gmail.com>
2022-08-03 11:55:29 +0900

    주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- jpashop2/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ jpashop2/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -71,4 +71,16 @@ public class OrderQueryRepository {
                 .collect(Collectors.toList());
         return orderIds;
     }
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                        "SELECT new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                                " FROM Order o" +
+                                " JOIN o.member m" +
+                                " JOIN o.delivery d" +
+                                " JOIN o.orderItems oi" +
+                                " JOIN oi.item i", OrderFlatDto.class)
+                .getResultList();
+
+    }
 }
