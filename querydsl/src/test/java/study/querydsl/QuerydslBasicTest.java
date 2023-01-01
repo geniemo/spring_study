e4ad867ca62af1f5ffe919f08ef541fd44413cf0
kor98won <kor98won@gmail.com>
2023-01-01 14:32:02 +0900

    프로젝션과 결과 반환 - 기본

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -458,4 +458,33 @@ public class QuerydslBasicTest {
             System.out.println("s = " + s);
         }
     }
+
+    @Test
+    public void simpleProjection() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+        // 한 컬럼만 들고오는 게 프로젝션 대상이 하나인 게 아니라 아래와 같이 멤버를 그대로 조회할 경우에드 프로젝션 대상이 하나인 것
+        // queryFactory.select(member).from(member).fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void tupleProjection() {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+        }
+    }
 }
